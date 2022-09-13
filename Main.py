6cdf8301a29c30fc81d47c4e993dc7dd489e524b
manoshika-catherine <110010617+manoshika-catherine@users.noreply.github.com>
2022-09-13 11:29:32 +0530

    mc

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ def insertionSort(array) -> List[int]:
   for i in range( 1, len(array):
                  t = array[i]
                  j = i-1
-                 while j >=0 and t < array[j] :
+                 while j >=0 and t < array[j]:
                        array[j+1] = array[j]
                        j -= 1
                  array[j+1] = t
