1cef03a781b108090b341a11b79e16dbb2fb937d
manoshika-catherine <110010617+manoshika-catherine@users.noreply.github.com>
2022-09-13 11:27:48 +0530

    mc

--- Main.py
+++ Main.py
@@ -1,13 +1,13 @@
 from typing import List
 
 def insertionSort(array) -> List[int]:
-  for i in range( 1, len ( array ):
-                 t=array[i]
-                 j=i-1
-                 while j>=0 and t< array [j] :
-                       array [j+1] = array [j]
-                       j -=1
-                 array [j+1] = t
+  for i in range( 1, len(array):
+                 t = array[i]
+                 j = i-1
+                 while j >=0 and t < array[j] :
+                       array[j+1] = array[j]
+                       j -= 1
+                 array[j+1] = t
   return array
               
 
